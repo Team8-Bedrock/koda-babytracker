b3dc491c70cc7a469471ec097e850b82579a90b5
MariaZunigaVega <zunigavegamaria@gmail.com>
2026-04-05 18:07:00 -0500

    Create activity.js with SleepSchema definition

---
+++ koda-server/model/activity.js
@@ -0,0 +1,18 @@
+/*
+
+const mongoose = require('mongoose');
+
+const SleepSchema = new mongoose.Schema({
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    duration: { type: Number, required: true }, // Duration in minutes
+    type: { type: String, enum: ['Outside', 'Inside', 'Car', 'Stroller'] }, // Optional, team decision
+    timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Sleep', SleepSchema);
+
+*/
+
+//CONCEPT
+//ADD LATER
