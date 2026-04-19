7260c685a62d8e997d62e5a66834d6f44cb75e49
Jacinta Thomas <jacintathomas@my.unt.edu>
2026-04-19 16:57:54 -0500

    Revert "update server.js for render deployment"
    This reverts commit 51f39f768ecbc4f7f44225a5cec72d3b4dd79b8c.

--- koda-server/server.js
+++ koda-server/server.js
@@ -9,10 +9,9 @@ const activitiesRouter = require('./routes/activities'); //mdz0019 import activi
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000", "https://koda-app-yy9x.onrender.com"]
-}));
-app.use('/api', activitiesRouter);
+app.use(cors());
+
+app.use('/api', activitiesRouter); //mdz0019 use activities routes
 
 // Connect to MongoDB using the secret variable
 mongoose.connect(process.env.MONGO_URI)
